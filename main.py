4d854753994b23a112a6619b4025a1a823338b0d
Вадим Воробьев <vadimvorobev@Vadims-MacBook-Pro.local>
2022-01-24 15:01:12 +0300

    Canny

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ def frame_conversion(img, x):
     return cv.resize(img, (img.shape[1]//x, img.shape[0]//x))
 
 def find_corners_on_img(img):
-    gray = np.float32(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
+
+    gray = np.float32(cv.cvtColor(cv.dilate(img, np.ones((5,5))), cv.COLOR_BGR2GRAY))
+    gray = cv.Canny(np.uint8(gray),100,200)
     coordinates = cv.cornerHarris(gray,2,3,0.04)
-    #dst = cv.dilate(coordinates, None)
-    img[coordinates>0.01*coordinates.max()]=[0,0,255]
+    dst = cv.dilate(coordinates, None)
+    img[dst>0.4*dst.max()]=[0,0,255]
 
 
 if __name__ == "__main__":
